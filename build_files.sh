4ecf5b5bcc0d4bdc2407feddebf7cd123bd117f5
refat7321-51 <refat7321@gmail.com>
2026-05-17 15:40:21 +0600

    Fix externally-managed-environment by adding --break-system-packages flag to pip install

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "==> Starting Build Process..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
 echo "==> Build Process Finished!"
